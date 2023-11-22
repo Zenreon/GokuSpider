18f0eea8996888deb39d6d1c6323f79c9728cdad
Zenreon <88670935+Zenreon@users.noreply.github.com>
2023-11-22 12:45:09 -0600

    Update EnemyTypes.cs

--- Patches/EnemyTypes.cs
+++ Patches/EnemyTypes.cs
@@ -1,25 +1,25 @@
-﻿using HarmonyLib;
+using HarmonyLib;
 using System;
 using System.Collections.Generic;
 using System.Text;
 using LC_API;
 using UnityEngine;
 
-namespace Gok.Patches
+namespace ArachnophobiaMod.patches
 {
     [HarmonyPatch]
     class EnemyTypes
     {
-        [HarmonyPatch(typeof(FlowermanAI), "Start")]
+        [HarmonyPatch(typeof(SandSpiderAI), "Start")]
         [HarmonyPostfix]
-        public static void  SummonGok(FlowermanAI __instance)
+        public static void SummonGok(SandSpiderAI __instance)
         {
-            __instance.creatureAngerVoice.clip = LC_API.BundleAPI.BundleLoader.GetLoadedAsset<AudioClip>("assets/ultra.mp3");
-            __instance.crackNeckSFX = LC_API.BundleAPI.BundleLoader.GetLoadedAsset<AudioClip>("assets/mods.mp3");
-            __instance.crackNeckAudio.clip = LC_API.BundleAPI.BundleLoader.GetLoadedAsset<AudioClip>("assets/mods.mp3");
-            GameObject.Destroy(__instance.gameObject.transform.Find("FlowermanModel").Find("LOD1").gameObject.GetComponent<SkinnedMeshRenderer>());
+            
+            GameObject.Destroy(__instance.gameObject.transform.Find("MeshContainer/MeshRenderer").gameObject.GetComponent<SkinnedMeshRenderer>());
             GameObject Gok = UnityEngine.Object.Instantiate(LC_API.BundleAPI.BundleLoader.GetLoadedAsset<GameObject>("assets/Gok.prefab"), __instance.gameObject.transform);
             Gok.transform.localPosition = new Vector3(0f, 1.5f, 0f);
+            
+            
         }
     }
 }
